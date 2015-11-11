8b590e7143cdc243daf70b0c7a72e6c371e449f8
Peter Rado <rado.peti@gmail.com>
2015-11-11 14:13:06 +0100

    Project FlowerCollector started
    Project goals:
    1. Create a board with tiles
    2. place the character on a tile,
    3 .move the character by clicking a tile next to the character.

---
+++ FlowerCollector1/FlowerCollector1/Character.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FlowerCollector1
+{
+    class Character
+    {
+        #region Fields
+        #endregion
+
+        #region Constructor
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Public methods
+        #endregion
+
+        #region Private methods
+        #endregion
+    }
+}
